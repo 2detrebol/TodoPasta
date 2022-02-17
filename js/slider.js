ba6291a84a6ad8a8aba9c9934a331b3658d1e73d
2detrebol <maubrandi@gmail.com>
2022-02-16 23:09:03 -0300

    acomodado footer en todas las pags + witdh text area y otros

--- js/slider.js
+++ js/slider.js
@@ -4,56 +4,55 @@ let currentSlide = 1;
 
 //Javascript for image slider manual navigation//
 var manualNav = function (manual) {
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-            btns.forEach((btn) => {
-                btn.classList.remove("active");
-            });
-        });
-        slides[manual].classList.add("active");
-        btns[manual].classList.add("active");
-    };
-    btns.forEach((btn, i) => {
-        btn.addEventListener("click", () => {
-        manualNav(i);
-        currentSlide = i;
-        });
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
     });
+  });
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
 
 /* Javascript for image slider autoplay navigation*/
 var repeat = function (activeClass) {
-    let active = document.getElementsByClassName("active");
-    let i = 1;
-    
-        var repeater = () => {
-        let tiempo
-        if (active[0]["children"][0].className === "vid") { //verifica si el elemento activo es un video
-        tiempo=58000; //en caso afirmativo asigna un tiempo de 59seg
-        active[0].firstElementChild.currentTime=0; //y vuelve al inicio el video
-        } 
-        else {
-            tiempo=3500; //en caso negativo asigna un tiempo de 3.5seg
-            }
-            setTimeout(function () {
-            [...active].forEach((activeSlide) => {
-              activeSlide.classList.remove("active");
-            });
+  let active = document.getElementsByClassName("active");
+  let i = 1;
 
-        slides[i].classList.add("active");
-        btns[i].classList.add("active");
-        i++;
-        console.log(i)
+  var repeater = () => {
+    let tiempo;
+    if (active[0]["children"][0].className === "vid") {
+      //verifica si el elemento activo es un video
+      tiempo = 58000; //en caso afirmativo asigna un tiempo de 59seg
+      active[0].firstElementChild.currentTime = 0; //y vuelve al inicio el video
+    } else {
+      tiempo = 3500; //en caso negativo asigna un tiempo de 3.5seg
+    }
+    setTimeout(function () {
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove("active");
+      });
 
-        if (slides.length == i) {
-           i = 0;
-        }
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
 
-        if (i >= slides.length) {
-           return;
-        }
-            repeater();
-        },tiempo);   //el tiempo va definido por la variable tiempo       
-        };
-        repeater();
-    };
-   repeat();
+      if (slides.length == i) {
+        i = 0;
+      }
+
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, tiempo); //el tiempo va definido por la variable tiempo
+  };
+  repeater();
+};
+repeat();
